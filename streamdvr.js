3afa8b58d7fd2815584b30fc60a68265e0cfc634
jrudess <jrudess>
2018-01-04 17:51:23 -0800

    Code cleanup

--- streamdvr.js
+++ streamdvr.js
@@ -3,30 +3,28 @@
 require("events").EventEmitter.prototype._maxListeners = 100;
 
 // 3rd Party Libraries
-const Promise = require("bluebird");
-const fs      = require("fs");
-const util    = require("util");
-const yaml    = require("js-yaml");
-const path    = require("path");
+const Promise  = require("bluebird");
+const fs       = require("fs");
+const util     = require("util");
+const yaml     = require("js-yaml");
+const path     = require("path");
 
 // local libraries
-const TUI     = require("./tui");
-const MFC     = require("./mfc");
-const CB      = require("./cb");
-const TWITCH  = require("./twitch");
+const TUI      = require("./tui");
+const MFC      = require("./mfc");
+const CB       = require("./cb");
+const TWITCH   = require("./twitch");
 
-const logFile = fs.createWriteStream(path.resolve() + "/streamdvr.log", {flags: "w"});
-const config  = yaml.safeLoad(fs.readFileSync("config.yml", "utf8"));
-const total   = Number(config.enableMFC) + Number(config.enableCB) + Number(config.enableTwitch);
-const tui     = new TUI.Tui(config);
+const logFile  = fs.createWriteStream(path.resolve() + "/streamdvr.log", {flags: "w"});
+const config   = yaml.safeLoad(fs.readFileSync("config.yml", "utf8"));
+const tui      = new TUI.Tui(config);
 
 console.log = function(msg) {
     logFile.write(util.format(msg) + "\n");
 };
 
 function mainSiteLoop(site) {
-
-    Promise.try(() => site.checkFileSize(config.captureDirectory, config.maxByteSize)
+    Promise.try(() => site.checkFileSize(site.config.captureDirectory, site.config.maxByteSize)
     ).then(() => site.processUpdates()
     ).then((bundle) => site.updateStreamers(bundle, 1)
     ).then((bundle) => site.updateStreamers(bundle, 0)
@@ -36,20 +34,17 @@ function mainSiteLoop(site) {
         site.errMsg(err);
         // throw err;
     }).finally(() => {
-        site.dbgMsg("Done, waiting " + config.scanInterval + " seconds.");
-        setTimeout(() => { mainSiteLoop(site); }, config.scanInterval * 1000);
+        site.dbgMsg("Done, waiting " + site.config.scanInterval + " seconds.");
+        setTimeout(() => { mainSiteLoop(site); }, site.config.scanInterval * 1000);
     });
 }
 
 function createSites() {
-    let inst = 1;
-
     tui.loadConfig();
 
     if (config.enableMFC) {
-        const mfc = new MFC.Mfc(config, tui, inst, total);
+        const mfc = new MFC.Mfc(config, tui);
         tui.addSite(mfc);
-        inst++;
         Promise.try(() => mfc.connect()).then(() => {
             mainSiteLoop(mfc);
         }).catch((err) => {
@@ -59,16 +54,14 @@ function createSites() {
     }
 
     if (config.enableCB) {
-        const cb = new CB.Cb(config, tui, inst, total);
+        const cb = new CB.Cb(config, tui);
         tui.addSite(cb);
-        inst++;
         mainSiteLoop(cb);
     }
 
     if (config.enableTwitch) {
-        const twitch = new TWITCH.Twitch(config, tui, inst, total);
+        const twitch = new TWITCH.Twitch(config, tui);
         tui.addSite(twitch);
-        inst++;
         mainSiteLoop(twitch);
     }
 
